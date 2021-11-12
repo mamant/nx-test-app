a917987113240dd93b70677e846ca1b3945dd435
mamant <anton.mamant@gmail.com>
2021-11-12 11:34:05 +0200

    remove unused

--- apps/contracts-list-app/src/app/app.js
+++ apps/contracts-list-app/src/app/app.js
@@ -1,21 +1,18 @@
-import styles from './app.module.scss';
 import ContractsList from './components/ContractsList';
 import { createBrowserHistory } from 'history';
 import ContractDetails from './components/ContractDetails';
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export function App() {
   const history = createBrowserHistory();
   return (
-    <div className="app">
-      <BrowserRouter history={history}>
-        <Routes>
-          <Route path="/" element={<ContractsList />} />
-          <Route path="/contracts" element={<ContractsList />}></Route>
-          <Route path="/contracts/:id" element={<ContractDetails />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter history={history}>
+      <Routes>
+        <Route path="/" element={<ContractsList />} />
+        <Route path="/contracts" element={<ContractsList />}></Route>
+        <Route path="/contracts/:id" element={<ContractDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
